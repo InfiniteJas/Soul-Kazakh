f7f1b49ff5b1baf8a27ad7f9507317e075df1c4f
InfiniteJas <92451144+InfiniteJas@users.noreply.github.com>
2023-05-26 19:51:28 +0600

    Update models.py

--- base/models.py
+++ base/models.py
@@ -2,11 +2,34 @@ from django.db import models
 
 # Create your models here.
 
-class Room(models.Model):
-  name = models.CharField(max_length=200)
-  description = models.TextField(null=True, blank=True)
-  updated = models.DateTimeField(auto_now=True)
-  created = models.DateTimeField(auto_now_add=True)
+class User(AbstractUser):
+  username = models.CharField(max_length=200)
+  email = models.EmailField(unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+    
+class Question(models.Model):
+  room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
 
+class Answer(models.Model):
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  content = models.TextField()
+  is_correct = models.BooleanField()
+
+
+class Main(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Room(models.Model):
+  room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=100)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  
   def __str__(self):
     return self.name
